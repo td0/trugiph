65fd126ed2d2ad647db6bb2a740e61ede36bda3c
Tadho <tadho@me.com>
2025-08-16 13:22:14 +0700

    chore: only show router devtools on DEV environment
    Signed-off-by: Tadho <tadho@me.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,19 +1,19 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { MainLayout } from '@/layout/MainLayout'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { MainLayout } from "@/layout/MainLayout";
 
 export const Route = createRootRoute({
   component: RootComponent,
   notFoundComponent: () => {
-    return <div>This is the global not found component</div>
+    return <div>This is the global not found component</div>;
   },
-})
+});
 
 function RootComponent() {
   return (
     <MainLayout>
       <Outlet />
-      <TanStackRouterDevtools />
+      {import.meta.env.DEV && <TanStackRouterDevtools />}
     </MainLayout>
-  )
+  );
 }
